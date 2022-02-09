0757271009dbefd39fb0aed42870bc17ba94f732
OllieSAE <1020925@student.sae.edu.au>
2022-02-09 14:15:39 +1100

    ColourButtonGame
    Version 1 implemented

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    public Color[] colours;
+    public string[] names;
+
+    public GameObject buttonLeft;
+    public GameObject buttonRight;
+    public GameObject P1Score;
+    public GameObject P2Score;
+
+    private bool colourOff = true;
+    private bool nameOff;
+
+    private int newColourIndex;
+    private int newTextIndex;
+
+    private int currentColour;
+    private int currentText;
+
+    private bool pointsAwarded = false;
+    private int playerOnePoints;
+    private int playerTwoPoints;
+    
+    void Start()
+    {
+        SetupArrays();
+    }
+
+    void Update()
+    {
+        UpdateScoreButtons();
+        ChangeButtons();
+        CheckButtons();
+        PlayerInputs();
+    }
+
+    void SetupArrays()
+    {
+        colours = new Color[6];
+        names = new string[6];
+
+        colours[0] = new Color(1,0,0,1);
+        colours[1] = new Color(0,1,0,1);
+        colours[2] = new Color(0,0,1,1);
+        colours[3] = new Color(1,0.92f,0.016f,1);
+        colours[4] = new Color(0,1,1,1);
+        colours[5] = new Color(1,0,1,1);
+        
+        names[0] = "red";
+        names[1] = "green";
+        names[2] = "blue";
+        names[3] = "yellow";
+        names[4] = "cyan";
+        names[5] = "magenta";
+        
+        foreach (Color colour in colours)
+        {
+            //print("colour " + colour);
+        }
+        foreach (string name in names)
+        {
+            //print("name " + name);
+        }
+    }
+
+    void ChangeButtons()
+    {
+        if (colourOff)
+        {
+            colourOff = false;
+            StartCoroutine(ChangeCoroutine());
+        }
+    }
+
+    void CheckButtons()
+    {
+        GetColour();
+        GetText();
+    }
+
+    void PlayerInputs()
+    {
+        if(currentColour == currentText)
+        {
+            if((Input.GetKeyDown("m")) && (!pointsAwarded))
+            {
+                pointsAwarded = true;
+                playerTwoPoints += 1;
+                //print("Player Two Points = " + playerTwoPoints);
+            }
+
+            if ((Input.GetKeyDown("a")) && (!pointsAwarded))
+            {
+                pointsAwarded = true;
+                playerOnePoints += 1;
+                //print("Player One Points = " + playerOnePoints);
+            }
+        }
+    }
+    
+    IEnumerator ChangeCoroutine()
+    {
+        yield return new WaitForSeconds(1.5f);
+        SetColour();
+        SetText();
+        colourOff = true;
+        pointsAwarded = false;
+    }
+    void SetColour()
+    {
+         newColourIndex = Random.Range(0, colours.Length - 1);
+         buttonLeft.GetComponent<Image>().color = colours[newColourIndex];
+         buttonRight.GetComponent<Image>().color = colours[newColourIndex];
+    }
+
+    void SetText()
+    {
+        newTextIndex = Random.Range(0, names.Length - 1);
+        //newText = names[2];
+        buttonLeft.GetComponentInChildren<TextMeshProUGUI>().text = names[newTextIndex];
+        buttonRight.GetComponentInChildren<TextMeshProUGUI>().text = names[newTextIndex];
+    }
+
+    void GetColour()
+    {
+        //currentColour = buttonLeft.GetComponent<Image>().color;
+        currentColour = newColourIndex;
+    }
+
+    void GetText()
+    {
+        //currentText = buttonLeft.GetComponentInChildren<TextMeshProUGUI>().text;
+        currentText = newTextIndex;
+    }
+
+    void UpdateScoreButtons()
+    {
+        P1Score.GetComponentInChildren<TextMeshProUGUI>().text = playerOnePoints.ToString();
+        P2Score.GetComponentInChildren<TextMeshProUGUI>().text = playerTwoPoints.ToString();
+    }
+}
